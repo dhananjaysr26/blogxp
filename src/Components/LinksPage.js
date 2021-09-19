b18a2de621b8567d28782d10ae203acabf1ee43c
Dhananjay Singh <dhananjaysr26@gmail.com>
2021-09-19 23:57:38 +0530

    feat: Added new Page Links Page and Post Page

--- src/Components/LinksPage.js
+++ src/Components/LinksPage.js
@@ -1,9 +1,50 @@
 import React from 'react'
-
+import "./LinksPage.css"
+import { GoLinkExternal } from "react-icons/go";
 function LinksPage() {
     return (
         <div>
-            <h1 style={{ marginTop: "125px" }}>I am Post Links Page </h1>
+
+            <div className="links">
+                <div className="link-single">
+                    <p className="link-text">Total link with Any Issue</p>
+                    <p className="link-num">100</p>
+                </div>
+                <div className="link-single">
+                    <p className="link-text">Last Check Completed At</p>
+                    <p className="link-num">20/08/2021</p>
+                </div>
+                <div className="link-single">
+                    <p className="link-text">Check Frequency</p>
+                    <p className="link-num">Daily</p>
+                </div>
+            </div>
+
+            <div className="link-main-container">
+                <div className="linktsub-container ">
+                    <h1>Broken Internal Links</h1>
+                    <ul className="chart">
+                        <li className="chart-list link-overflow">https://stackoverflow.com/questions/57456743/how-should-i-split-grids-in-css
+                            <GoLinkExternal className="elink-icons" />
+                            <span>5 times Used</span></li>
+                        <li className="chart-list link-overflow">https://stackoverflow.com/questions/57456743/how-should-i-split-grids-in-css <span>5 times Used</span></li>
+                        <li className="chart-list link-overflow">https://stackoverflow.com/questions/57456743/how-should-i-split-grids-in-css <span>5 times Used</span></li>
+                        <li className="chart-list link-overflow">https://stackoverflow.com/questions/57456743/how-should-i-split-grids-in-css <span>5 times Used</span></li>
+
+                    </ul>
+                </div>
+                <div className="linktsub-container ">
+                    <h1>Broken External Links</h1>
+                    <ul className="chart">
+                        <li className="chart-list link-overflow">https://stackoverflow.com/questions/57456743/how-should-i-split-grids-in-css
+                            <GoLinkExternal className="elink-icons" />
+                            <span>5 times Used</span></li>
+                        <li className="chart-list link-overflow">https://stackoverflow.com/questions/57456743/how-should-i-split-grids-in-css <span>5 times Used</span></li>
+                        <li className="chart-list link-overflow">https://stackoverflow.com/questions/57456743/how-should-i-split-grids-in-css <span>5 times Used</span></li>
+
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
--- src/Components/PostPage.js
+++ src/Components/PostPage.js
@@ -1,9 +1,76 @@
 import React from 'react'
+import './PostPage.css'
 
+import { GoLinkExternal } from "react-icons/go";
+import { BiEdit } from "react-icons/bi";
 function PostPage() {
     return (
-        <div>
-            <h1 style={{ marginTop: "125px" }}>I am Post Page</h1>
+        <div className="post-main-container">
+            <div className="postsub-container">
+                <h1>Without Meta Discription</h1>
+                <ul className="chart">
+                    <li className="chart-list">My 10 Best Post in English
+                        <GoLinkExternal className="elink-icons" />
+                        <BiEdit className="edit-icons" />
+                        <span>mark as Valid</span></li>
+                    <li className="chart-list">My 10 Best Post in English <span>mark as Valid</span></li>
+                    <li className="chart-list">My 10 Best Post in English <span>mark as Valid</span></li>
+                    <li className="chart-list">My 10 Best Post in English <span>mark as Valid</span></li>
+
+                </ul>
+            </div>
+            <div className="postsub-container">
+                <h1>Without Featured Image</h1>
+                <ul className="chart">
+                    <li className="chart-list">My 10 Best Post in English
+                        <GoLinkExternal className="elink-icons" />
+                        <BiEdit className="edit-icons" />
+                        <span>mark as Valid</span></li>
+
+                    <li className="chart-list">My 10 Best Post in English <span>mark as Valid</span></li>
+
+                </ul>
+            </div>
+            <div className="postsub-container">
+                <h1>Too Short Post(Below 250words)</h1>
+                <ul className="chart">
+                    <li className="chart-list">My 10 Best Post in English
+                        <GoLinkExternal className="elink-icons" />
+                        <BiEdit className="edit-icons" />
+                        <span>mark as Valid</span></li>
+                    <li className="chart-list">My 10 Best Post in English <span>mark as Valid</span></li>
+                    <li className="chart-list">My 10 Best Post in English <span>mark as Valid</span></li>
+
+
+                </ul>
+            </div>
+            <div className="postsub-container">
+                <h1>Too Long Meta Discription</h1>
+                <ul className="chart">
+                    <li className="chart-list">My 10 Best Post in English
+                        <GoLinkExternal className="elink-icons" />
+                        <BiEdit className="edit-icons" />
+                        <span>mark as Valid</span></li>
+                    <li className="chart-list">My 10 Best Post in English <span>mark as Valid</span></li>
+                </ul>
+            </div>
+            <div className="postsub-container">
+                <h1>Too Long Url (more than 100 char)</h1>
+                <ul className="chart">
+                    <li className="chart-list">My 10 Best Post in English
+                        <GoLinkExternal className="elink-icons" />
+                        <BiEdit className="edit-icons" />
+                        <span>mark as Valid</span></li>
+
+                </ul>
+            </div>
+            <div className="postsub-container">
+                <h1>Wrong Slug</h1>
+                <ul className="chart">
+                    <p >Awesome Everything is Good Here!</p>
+                </ul>
+            </div>
+
         </div>
     )
 }
